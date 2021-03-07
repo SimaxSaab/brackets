9bd3500355e8dd46afed26f018b0f1c2aa08ced8
SimaxSaab <minoreat@gmail.com>
2021-03-07 10:07:55 +0200

    init: ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) return false;
+  let brackets = bracketsConfig.map((item) => item.join(''));
+  for (let i = 0, len = brackets.length; i < len; i++) {
+    while (str.includes(brackets[i])) {
+        str = str.replace(brackets[i], '');
+        i = -1;
+    }
+  }
+  return str === '' ? true : false;
 }
